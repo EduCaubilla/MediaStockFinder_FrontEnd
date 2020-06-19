184d341db1f6d2ed02b3118dab5cdffb36c0059a
Edu Caubilla <educaubilla@MacBook-Pro-de-Edu.local>
2020-06-19 13:07:37 +0200

    MVP_Server

--- src/app/shared/request.service.ts
+++ src/app/shared/request.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { PhotoInterface } from './photo-interface';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -18,47 +19,48 @@ export class RequestService {
 
 // BUSCADOR PRINCIPAL
   searchPhotos$(search) {
-    const URL_API_SEARCH = `http://localhost:9000/photo/${search}`;
+    // const URL_API_SEARCH = `http://localhost:9000/photo/${search}`;
+    const URL_API_SEARCH = `${environment.API_URL}/photo/${search}`;
     return this.http.get<PhotoInterface>(URL_API_SEARCH);
   }
 
 // RANDOM
   listRandom$() {
-    const URL_API_RANDOM = 'http://localhost:9000/photo/random';
+    const URL_API_RANDOM = `${environment.API_URL}/photo/random`;
     return this.http.get<PhotoInterface>(URL_API_RANDOM
     );
   }
 
 // CATEGORÍA NATURE
   searchPhotosNature$() {
-    const URL_API_NATURE = `http://localhost:9000/photo/categories/nature`;
+    const URL_API_NATURE = `${environment.API_URL}/photo/categories/nature`;
     return this.http.get<PhotoInterface>(URL_API_NATURE);
   }
 
 // CATEGORÍA PEOPLE
   searchPhotosPeople$() {
-    const URL_API_PEOPLE = `http://localhost:9000/photo/categories/people`;
+    const URL_API_PEOPLE = `${environment.API_URL}/photo/categories/people`;
     return this.http.get<PhotoInterface>(URL_API_PEOPLE);
   }
 
 // CATEGORÍA FOOD & DRINKS
 
   searchPhotosFoodDrink$() {
-    const URL_API_FOODDRINK = `http://localhost:9000/photo/categories/food&drink`;
+    const URL_API_FOODDRINK = `${environment.API_URL}/photo/categories/food&drink`;
     return this.http.get<PhotoInterface>(URL_API_FOODDRINK);
   }
 
 // CATEGORÍA LATEST
 
   searchPhotosLatest$() {
-    const URL_API_LATEST = `http://localhost:9000/photo/categories/latest`;
+    const URL_API_LATEST = `${environment.API_URL}/photo/categories/latest`;
     return this.http.get<PhotoInterface>(URL_API_LATEST);
   }
 
 // RANDOM PARA UNA FOTO - JUMBOTRON
 
   oneRandom$() {
-    const URL_API_ONERANDOM = 'http://localhost:9000/photo/onerandom';
+    const URL_API_ONERANDOM = `${environment.API_URL}/photo/onerandom`;
     return this.http.get<PhotoInterface>(URL_API_ONERANDOM);
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://media-stock-finder.herokuapp.com/photo/random'
 };
